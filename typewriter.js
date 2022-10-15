ecdc08dede98e59402c0e91f5efd9a1ee7408c2d
tronross <john_ross@analogforever.ca>
2022-10-14 22:02:21 -0400

    Implemented for loop incrementing setTimeout delay values

--- typewriter.js
+++ typewriter.js
@@ -1,13 +1,19 @@
+// FUNCTION
+// typewriter: simulates typing out a sentence by way of incremented setTimeout delay values
 const sentence = "while the digital revolution in music streaming is sublime, vinyl is still king";
 let typeCount = 0;
+let feed = sentence + "\n";
 
 const type = function() {
-  if (typeCount < sentence.length) {
-  process.stdout.write(sentence.charAt(typeCount));
-  typeCount++;
+  const stop = feed.length + 1;
+  for (let i = 0; i < stop; i++) {
+    if (i < feed.length) {
+      let delay = (i * 120)
+      setTimeout(() => {
+      process.stdout.write(feed.charAt(i));
+    }, delay);
+  }
 }
 }
 
-const typeOut = function() {
-  setInterval(type(), 120);
-};
+type();
