56cb010b039f2ab2e985493e3b09a0181d6c0920
tronross <john_ross@analogforever.ca>
2022-10-14 22:03:45 -0400

    Cleaned and linted

--- typewriter.js
+++ typewriter.js
@@ -1,19 +1,18 @@
 // FUNCTION
 // typewriter: simulates typing out a sentence by way of incremented setTimeout delay values
 const sentence = "while the digital revolution in music streaming is sublime, vinyl is still king";
-let typeCount = 0;
 let feed = sentence + "\n";
 
 const type = function() {
   const stop = feed.length + 1;
   for (let i = 0; i < stop; i++) {
     if (i < feed.length) {
-      let delay = (i * 120)
+      let delay = (i * 120);
       setTimeout(() => {
-      process.stdout.write(feed.charAt(i));
-    }, delay);
+        process.stdout.write(feed.charAt(i));
+      }, delay);
+    }
   }
-}
-}
+};
 
 type();
